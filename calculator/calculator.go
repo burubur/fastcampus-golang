183ae48e9051c4eff8a52bd1686ec25c2bded75a
Burhan Mubarok <burhanmubarok@gmail.com>
2024-04-13 17:05:25 +0700

    CH07-03. Test Assertion Framework - Testify

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,15 @@
 package calculator
 
+import "errors"
+
 func Add(a, b int) int {
 	return a + b
+}
+
+func DivideBy(a, b int) (int, error) {
+	if b > a {
+		return 0, errors.New("too big number")
+	}
+
+	return a / b, nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -4,12 +4,32 @@ import (
 	"testing"
 
 	"github.com/burubur/fastcampus-golang/calculator"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAdd(t *testing.T) {
 	result := calculator.Add(1, 2)
 	expected := 3
-	if result != expected {
-		t.Errorf("Add(1, 2) should return 3 but got %d", result)
-	}
+
+	assert.Equal(t, expected, result, "Add(1, 2) should return 3")
+}
+
+func TestDivideBy(t *testing.T) {
+	result, err := calculator.DivideBy(10, 2)
+	assert.NoError(t, err, "it should not return error")
+
+	assert.Equal(t, 5, result, "DivideBy(10, 2) should return 5")
+}
+
+func TestDivideBy_BiggerNumber(t *testing.T) {
+	result, err := calculator.DivideBy(2, 10)
+	assert.Error(t, err, "it should return error")
+
+	assert.Equal(t, 0, result, "DivideBy(2, 10) should return 0 due to invalid number")
+}
+
+func TestDivideBy_ZeroNumber(t *testing.T) {
+	assert.NotPanics(t, func() {
+		_, _ = calculator.DivideBy(0, 10)
+	}, "It should not panic even if we send an invalid number, at least it should return 0")
 }
